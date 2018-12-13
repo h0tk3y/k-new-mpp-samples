2ba1faf44ef44cbefeb3edc68c4a9a26a14106e9
Sergey Igushkin <hotkeytlt@gmail.com>
2018-12-14 01:20:09 +0300

    Update Kotlin to 1.3.20-eap-25, Gradle to 5.0

--- lib-and-app/sample-app/src/allJvm/kotlin/useBothIds.kt
+++ lib-and-app/sample-app/src/allJvm/kotlin/useBothIds.kt
@@ -1,9 +1,8 @@
 package com.github.h0tk3y.kotlinMppDemo.app
 
-import com.github.h0tk3y.kotlinMppDemo.lib.*
+import com.github.h0tk3y.kotlinMppDemo.lib.id
 
 fun main(args: Array<String>) {
     println(id(123))
     println(idUsage(456))
-    println(x())
 }
